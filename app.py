60c990c86a2f5290d029168c9eb396a0fa3e0c02
mahadnav <mahadn20@gmail.com>
2025-07-17 21:59:34 +0500

    ''

--- app.py
+++ app.py
@@ -244,7 +244,6 @@ with tabs[3]:
                         .result-box {
                             background-color: #FFD43B;
                             border-radius: 10px;
-                            padding: -10;
                             text-align: center;
                             min-height: 275px;
                             display: flex;
