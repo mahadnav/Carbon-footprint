5e111a459038b425a11c23719539b052192dcf9b
mahadnav <mahadn20@gmail.com>
2025-07-18 04:30:51 +0500

    ''

--- app.py
+++ app.py
@@ -242,9 +242,13 @@ with tabs[3]:
         </style>
     """, unsafe_allow_html=True)
 
-    st.markdown("<div class='main-title'>🎉 Congratulations!</div>", unsafe_allow_html=True)
-    st.markdown("<div class='subtitle'>Your annual footprint is close to the national average, but there's still room to improve!</div>", unsafe_allow_html=True)
-
+    if total_emissions < 2.1:
+        st.markdown("<div class='main-title'>🎉 Congratulations!</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>Your annual footprint is less that the national average!</div>", unsafe_allow_html=True)
+    else:
+        st.markdown("<div class='main-title'>❗ Oops!</div>", unsafe_allow_html=True)
+        st.markdown("<div class='subtitle'>Your annual footprint is above the national average. Let's work on reducing it!</div>", unsafe_allow_html=True)
+    
     col1, col2, col3 = st.columns([2, 1.5, 1.5])
     with col1:
         st.markdown("""
