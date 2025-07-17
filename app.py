6ed117cefd051879bb58f1ee1226c2489c6bc657
mahadnav <mahadn20@gmail.com>
2025-07-17 18:56:15 +0500

    ''

--- app.py
+++ app.py
@@ -50,12 +50,12 @@ with tabs[0]:
     with st.expander("➕ Enter your household energy usage"):
         col1, col2 = st.columns(2)
         with col1:
-            user_data['electricity'] = st.number_input("Electricity (kWh/year)", min_value=0, value=None, placeholder="e.g. 1200", format="%d")
+            user_data['electricity'] = st.number_input("Electricity (kWh/year)", min_value=0, value=None, placeholder="e.g. 10,000", format="%d")
         with col2:
-            user_data['gas'] = st.number_input("Natural Gas (m³/year)", min_value=0, value=None, format="%d")
+            user_data['gas'] = st.number_input("Natural Gas (m³/year)", min_value=0, value=None, placeholder='e.g. 5,000', format="%d")
 
     if user_data['electricity'] is None or user_data['gas'] is None:
-        st.warning("Please enter both electricity and gas usage to calculate household emissions.")
+        st.text("Please enter both electricity and gas usage to calculate household emissions.")
     else:
         household_emissions = calculate_emissions(user_data)[0]['Household'] / 1000
         st.metric(label="Household Emissions", value=f"{household_emissions:,.2f} metric tons CO₂")
