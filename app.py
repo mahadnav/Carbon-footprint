bf74c924963a68ce648651b6a1af077856d826b3
mahadnav <mahadn20@gmail.com>
2025-07-24 22:39:10 +0500

    ''

--- app.py
+++ app.py
@@ -316,11 +316,11 @@ with tabs[1]:
                     st.markdown(f"**Leg {i + 1}**")
                     col1, col2, col3 = st.columns([4, 4, 2])
                     with col1:
-                        dep = st.selectbox(f"Departure Airport (Leg {i + 1})", options=sorted(list(airports.keys())), index=None, placeholder='Choose your departure airport', key=f"dep_{i}")
+                        dep = st.selectbox(f"Departure City (Leg {i + 1})", options=sorted(list(airports.keys())), index=None, placeholder='Choose your departure city', key=f"dep_{i}")
                     with col2:
                         # Filter out the selected departure airport from the arrival options
                         arrival_options = sorted([airport for airport in airports.keys() if airport != dep])
-                        arr = st.selectbox(f"Arrival Airport (Leg {i + 1})", options=arrival_options, index=None, placeholder="Choose your arrival airport", key=f"arr_{i}")
+                        arr = st.selectbox(f"Arrival City (Leg {i + 1})", options=arrival_options, index=None, placeholder="Choose your arrival city", key=f"arr_{i}")
                     with col3:
                         st.markdown("<div style='height: 35px;'></div>", unsafe_allow_html=True)  # Empty space for alignment
                         is_round = st.checkbox("Return?", key=f"return_{i}", value=True)
