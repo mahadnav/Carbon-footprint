9ccca01a7bd7f1728f75ac3acd047f1cea5dcb93
mahadnav <mahadn20@gmail.com>
2025-07-24 20:30:07 +0500

    ''

--- app.py
+++ app.py
@@ -381,7 +381,7 @@ with tabs[3]:
                             background-color: #FFD43B;
                             border-radius: 10px;
                             text-align: center;
-                            min-height: 320px;
+                            min-height: 319px;
                             display: flex;
                             flex-direction: column;
                             justify-content: center;
