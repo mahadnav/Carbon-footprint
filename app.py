150ae0527732432a7caa38690077a4f8346ed29e
mahadnav <mahadn20@gmail.com>
2025-07-26 04:34:39 +0500

    ''

--- app.py
+++ app.py
@@ -626,8 +626,8 @@ with tabs[3]:
                     
         st.markdown(f"""
             <div class='result-box'>
-                <h2 style='font-size: 32px;'><b>Your Annual Carbon Footprint</b></h2>
-                <div style='font-size: 50px; font-weight: bold;'>{total_emissions}
+                <h2 style='font-size: 24px;'><b>Your Annual Carbon Footprint</b></h2>
+                <div style='font-size: 65px; font-weight: bold;'>{total_emissions}
                 <span style='font-size: 24px; font-weight: normal;'> tCO₂e</span>
                 </div>
             </div>
