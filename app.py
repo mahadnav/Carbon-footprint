95ea41c0ef285b0704934f9fcd1a3566659b71a8
mahadnav <mahadn20@gmail.com>
2025-07-25 23:53:42 +0500

    ''

--- app.py
+++ app.py
@@ -615,7 +615,7 @@ with tabs[3]:
             <div class='category-box' style='background-color: #1A237E;'>
                 <div style='font-size: 24px; color: #fafafa;'><b>⚡ Household Energy</b></div>
                 <div style='color: #fafafa;'>Your consumption is equal to <b>{household_emissions:.2f} tonnes CO₂e</b></div>
-                <div style='height: 12px;'></div>
+                <div style='height: 10px;'></div>
             </div>
         """, unsafe_allow_html=True)
 
@@ -625,7 +625,7 @@ with tabs[3]:
             <div class='category-box' style='background-color: #1B5E20;'>
                 <div style='font-size: 24px; color: #ffffff;'><b>🚗 Transport</b></div>
                 <div style='color: #fafafa;'>Your consumption is equal to <b>{vehicle_emissions:.2f} tonnes CO₂e</b></div>
-                <div style='height: 12px;'></div>
+                <div style='height: 10px;'></div>
             </div>
         """, unsafe_allow_html=True)
 
@@ -635,7 +635,7 @@ with tabs[3]:
             <div class='category-box' style='background-color: #AD1457;'>
                 <div style='font-size: 24px; color: #ffffff;'><b>🛒 Secondary</b></div>
                 <div style='color: #fafafa;'>Your consumption is equal to <b>{sec_emissions:.2f} tonnes CO₂e</b></div>
-                <div style='height: 12px;'></div>
+                <div style='height: 10px;'></div>
             </div>
         """, unsafe_allow_html=True)
 
