eccb6d37cd811726c0517cefc6dc3d8942730c9d
mahadnav <mahadn20@gmail.com>
2025-07-25 23:50:42 +0500

    ''

--- app.py
+++ app.py
@@ -614,7 +614,7 @@ with tabs[3]:
             <div style='height: 17px;'></div>
             <div class='category-box' style='background-color: #1A237E;'>
                 <div style='font-size: 24px; color: #ffffff;'><b>⚡ Household Energy</b></div>
-                <div style='color: #888888>Your consumption is equal to <b>{household_emissions:.2f} tonnes CO₂e</b></div>
+                <div style='color: #888888;'>Your consumption is equal to <b>{household_emissions:.2f} tonnes CO₂e</b></div>
             </div>
         """, unsafe_allow_html=True)
 
@@ -623,7 +623,7 @@ with tabs[3]:
             <div style='height: 17px;'></div>
             <div class='category-box' style='background-color: #1B5E20;'>
                 <div style='font-size: 24px; color: #ffffff;'><b>🚗 Transport</b></div>
-                <div style='color: #888888>Your consumption is equal to <b>{vehicle_emissions:.2f} tonnes CO₂e</b></div>
+                <div style='color: #888888;'>Your consumption is equal to <b>{vehicle_emissions:.2f} tonnes CO₂e</b></div>
             </div>
         """, unsafe_allow_html=True)
 
@@ -632,7 +632,7 @@ with tabs[3]:
             <div style='height: 17px;'></div>
             <div class='category-box' style='background-color: #AD1457;'>
                 <div style='font-size: 24px; color: #ffffff;'><b>🛒 Secondary</b></div>
-                <div style='color: #888888>Your consumption is equal to <b>{sec_emissions:.2f} tonnes CO₂e</b></div>
+                <div style='color: #888888;'>Your consumption is equal to <b>{sec_emissions:.2f} tonnes CO₂e</b></div>
             </div>
         """, unsafe_allow_html=True)
 
