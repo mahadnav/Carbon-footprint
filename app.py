ee29286215829d587f89b1d260cbaeba33d3a869
mahadnav <mahadn20@gmail.com>
2025-07-26 05:23:08 +0500

    ''

--- app.py
+++ app.py
@@ -659,7 +659,7 @@ with tabs[3]:
             <div style='height: 20px;'></div>
 
             <div class='grey-box'>
-                <div style='font-size: 16px;'>You're amongst Pakistan's top</div>
+                <div style='font-size: 16px;'>You're among Pakistan's top</div>
                 <div style='font-size: 36px; font-weight: bold;'>
                     {round(user_percentile(total_emissions), 1)}
                     <span style='font-size: 24px;'>%</span>
