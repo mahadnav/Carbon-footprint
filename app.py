c0f26427e26be72eeb6ba806d8c45bb4cd49ee90
mahadnav <mahadn20@gmail.com>
2025-07-26 05:36:21 +0500

    ''

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def user_percentile(total_emissions):
     np.random.seed(42)
 
     low_income = np.random.normal(loc=0.9, scale=0.3, size=5000)        # 50%
-    middle_income = np.random.normal(loc=2.5, scale=0.5, size=4000)     # 40%
+    middle_income = np.random.normal(loc=2.1, scale=0.5, size=4000)     # 40%
     high_income = np.random.normal(loc=9, scale=1.5, size=1000)         # 10%
 
     pakistan_emissions = np.concatenate([low_income, middle_income, high_income])
