cab0c78aead6eb937d6e68a54e60ed68d5e5e456
mahadnav <mahadn20@gmail.com>
2025-07-29 05:40:56 +0500

    ''

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def tabs_style():
             justify-content: center;
             overflow-x: auto;
             white-space: nowrap;
-            max-width: 95%;
+            max-width: 98%;
             border-radius: 20px;
             padding: 0;
             margin: auto;
