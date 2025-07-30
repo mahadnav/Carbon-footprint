cbe423f951f166140a088de92f57f9a88c0749fa
mahadnav <mahadn20@gmail.com>
2025-07-30 12:48:49 +0500

    ''

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ st.markdown("""
 #     </div>
 # """, unsafe_allow_html=True)
 
-st_invisible_container()
+# st_invisible_container()
 with st.container(border=True):
     tabs_style()         
     tabs = st.tabs(["Household", "Transport", "Secondary", "Total"])
